502f73ffe547c4ba9040ead40d3f24e00ed95971
Joshua Langelaar <93121879+DeCooleCoder@users.noreply.github.com>
2022-05-20 07:18:06 +0000

    test

--- main.py
+++ main.py
@@ -1,9 +1,10 @@
 #Idea 1: Your stranded from a time machine and are stranded between timelines So there may be dino's or medieval knights or dragons to conquer
 #FIZZBUZZ code
+#test
 for x in range(101):
   if x == 0:
       continue  
-  elif x % 5 == 0 and x % 3 == 0:
+  elif x % 6 == 0 and x % 3 == 0:
     print("FIZZ BUZZ")
   elif x % 3 == 0:
     print("FIZZ")
