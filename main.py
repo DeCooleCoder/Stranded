78e39375c31f0e512dc7930f2bfccf5e2bfa993d
Error734862947 <93121855+B2710@users.noreply.github.com>
2022-05-20 07:21:04 +0000

    Test end

--- main.py
+++ main.py
@@ -1,14 +1,1 @@
 #Idea 1: Your stranded from a time machine and are stranded between timelines So there may be dino's or medieval knights or dragons to conquer
-#FIZZBUZZ code
-#test
-for x in range(101):
-  if x == 0:
-      continue  
-  elif x % 6 == 0 and x % 3 == 0:
-    print("FIZZ BUZZ")
-  elif x % 3 == 0:
-    print("FIZZ")
-  elif x % 5 == 0:
-    print("BUZZ")
-  else: print (x)
-    #kan je dit pullen
