d3e74da0bfa0b0204400e33d9be5ed9bf2916d3b
Error734862947 <93121855+B2710@users.noreply.github.com>
2022-06-01 16:42:18 +0000

    Fixed the error

--- main.py
+++ main.py
@@ -8,7 +8,10 @@
 #   "options" : []
 # },
 
-# all the rooms!
+print("Please enter your name here:")
+name = input("- "),
+
+#all the rooms!
 rooms = {
   "Home" :  {
     "title" : "Home",
@@ -20,27 +23,25 @@ rooms = {
   "Wake up" :  {
       "title" : "You wake up in your bedroom and check your agenda. You see you made an appointment with the professors today to test their new invention out.",
       "description" : "You should head out right about now…you wouldn't want to be late at the lab...",
-      "extradialogue": "Note: Press S to Stay at Home and Press L to go to the Lab",
-      "options": ["S", "L"],
-      "S" = "Lab"
-      "L" = "Stay to Home"
+      "extradialogue": "Note: Press S to Stay at Home and Press N to go to the Lab",
+      "options": ["N / S"],
   },
 
-  "Stay to Home" : 
+  "S" : {
+      "title" : ""
+  },
 
-  "Lab" :  {
+  "N" :  {
     "title" : "The Lab",
     "description" : "You arrive at the Lab and see 2 doors waiting for you. Both have a sign. One saying “THE SMART AND AMAZING PROFESSOR ” and the other “The Man”. Which will you open first?",
     "extradialogue" : "Note: Type N for Open door THE SMART AND AMAZING PROFESSOR and Type M for Open door The Man",
     "options": ["N", "M"]
-    ""
   },
 
  "N" :  {
    "title" : "THE SMART AND AMAZING PROFESSOR's office",
    "description" : "Professor Hunter: Hello and welcome to our lab. I’m professor Hunter and this is where we do all of our very scientific experiments and take our jobs very seriously. We have never made a SINGLE mistake so you don’t have to worry about your safety ;) So… what was your name again?",
-    player.name = input("- "),
-   "extradialogue" : f"Professor Hunter: Riiight gotcha… so… {player.name} Have you met the other professor yet? Note: Type M to speak to professor Jerkmoose and Press N to continue talking to professor Hunter",
+   "extradialogue" : f"Professor Hunter: Riiight gotcha… so… {name} Have you met the other professor yet? Note: Type M to speak to professor Jerkmoose and Press N to continue talking to professor Hunter",
    "options" : ["M", "N"]
  },
     
@@ -61,7 +62,7 @@ def game(room):
   description = currentRoom["description"]  
   extradialogue = currentRoom["extradialogue"]
   options = currentRoom["options"] 
-  
+
   # show to user
   print(f"{title}")
   print()
